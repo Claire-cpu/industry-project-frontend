f65327bddf85bfe60443d9da3596d27d3a623ce2
Hannahsioco <hannahsioco@gmail.com>
2025-02-14 12:37:39 -0500

    feat: add header with logo and navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.scss";
 import QuestionCard from "./components/QuestionCard/QuestionCard";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <QuestionCard />
     </>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.scss"; 
+
+const Header = () => {
+  return (
+    <header className="header">
+
+      <div className="logo-container">
+        <img
+          src="https://static.cdnlogo.com/logos/m/6/microsoft.svg"
+          alt="Microsoft Logo"
+          className="logo"
+        />
+      </div>
+
+      <nav className="nav-links">
+        <a href="/" className="nav-link">
+          Play The Game
+        </a>
+        <span className="divider">|</span>
+        <a href="/about" className="nav-link">
+          Let Us Help
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
