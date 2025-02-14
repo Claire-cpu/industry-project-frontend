717616c1a79a4c0b5b95d5c7d737921db12df3bd
carazhang93 <133140681+carazhang93@users.noreply.github.com>
2025-02-14 11:49:48 -0500

    added carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.scss";
 import QuestionCard from "./components/QuestionCard/QuestionCard";
-
+import CarouselComponent from "./components/Carousel/CarouselComponent";
 function App() {
   return (
     <>
+      <CarouselComponent />
       <QuestionCard />
     </>
   );
