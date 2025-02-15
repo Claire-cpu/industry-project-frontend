5811eaa2c1773a53c6da0949908bb59570dbe4b9
carazhang93 <133140681+carazhang93@users.noreply.github.com>
2025-02-15 12:26:38 -0500

    updated style

--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -5,13 +5,11 @@ import "./QuestionCard.scss";
 import AnswerModal from "../../components/AnswerModal/AnswerModal";
 import Animation from "../Animation/Animation";
 
-import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const baseURL = import.meta.env.VITE_API_URL;
 
 function QuestionCard({ score, updateScore }) {
-
   const [selected, setSelected] = useState(null);
   const [flipped, setFlipped] = useState(false);
   const [questions, setQuestions] = useState([]);
@@ -81,62 +79,61 @@ function QuestionCard({ score, updateScore }) {
 
   return (
     <>
-      <Header /> 
       <div className="question-wrapper">
-    <section className={`question-card ${flipped ? "flipped" : ""}`}>
-      <div className="question-card__inner">
-        {!flipped ? (
-          <div className="question-card__front">
-            <div className="question-card__score">
-              <strong>Score: {score}</strong>
-            </div>
-            <h2 className="question-card__hint-title">Hint</h2>
-            <h3 className="question-card__hint">{currentQuestion?.hint}</h3>
+        <section className={`question-card ${flipped ? "flipped" : ""}`}>
+          <div className="question-card__inner">
+            {!flipped ? (
+              <div className="question-card__front">
+                <div className="question-card__score">
+                  <strong>Score: {score}</strong>
+                </div>
+                <h2 className="question-card__hint-title">Hint</h2>
+                <h3 className="question-card__hint">{currentQuestion?.hint}</h3>
+              </div>
+            ) : (
+              <div className="question-card__back">
+                <div className="question-card__score">
+                  <strong>Score: {score}</strong>
+                </div>
+                <Timer duration={30} onEnd={() => alert("Time's Up!")} />
+                <p className="question-card__question">
+                  {currentQuestion?.question}
+                </p>
+                <ul className="question-card__list">
+                  {shuffledAnswers.map((answer, index) => (
+                    <li
+                      key={index}
+                      className={`question-card__item ${
+                        selected === index
+                          ? answer === currentQuestion.correct_answer
+                            ? "question-card__item--correct"
+                            : "question-card__item--incorrect"
+                          : ""
+                      } ${answered ? "question-card__item--disabled" : ""}`}
+                      onClick={() => handleSelect(index)}
+                    >
+                      {answer}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
-        ) : (
-          <div className="question-card__back">
-            <div className="question-card__score">
-              <strong>Score: {score}</strong>
-            </div>
-            <Timer duration={30} onEnd={() => alert("Time's Up!")} />
-            <p className="question-card__question">
-              {currentQuestion?.question}
-            </p>
-            <ul className="question-card__list">
-              {shuffledAnswers.map((answer, index) => (
-                <li
-                  key={index}
-                  className={`question-card__item ${
-                    selected === index
-                      ? answer === currentQuestion.correct_answer
-                        ? "question-card__item--correct"
-                        : "question-card__item--incorrect"
-                      : ""
-                  } ${answered ? "question-card__item--disabled" : ""}`}
-                  onClick={() => handleSelect(index)}
-                >
-                  {answer}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
 
-        <div className="question-card__next-wrapper">
-          <button className="question-card__next" onClick={handleNext}>
-            {flipped ? "Next" : "Ready?"}
-          </button>
-        </div>
-        {isCorrect !== null && (
-          <AnswerModal
-            isCorrect={isCorrect}
-            correctAnswer={correctAnswer}
-            onClose={handleCloseModal}
-          />
-        )}
-      </section>
-      <Animation score={score} />
+          <div className="question-card__next-wrapper">
+            <button className="question-card__next" onClick={handleNext}>
+              {flipped ? "Next" : "Ready?"}
+            </button>
+          </div>
+          {isCorrect !== null && (
+            <AnswerModal
+              isCorrect={isCorrect}
+              correctAnswer={correctAnswer}
+              onClose={handleCloseModal}
+            />
+          )}
+        </section>
+        <Animation score={score} />
       </div>
       <Footer />
     </>
